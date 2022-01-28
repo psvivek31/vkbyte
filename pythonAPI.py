bab143bf21c218c1de0557af2aef11ffe87d3406
vivek <53150078+vivk-giri@users.noreply.github.com>
2022-01-28 14:35:08 +0530

    Update pythonAPI.py

--- pythonAPI.py
+++ pythonAPI.py
@@ -14,6 +14,6 @@ print(API_key)
 url = 'https://www.virustotal.com/vtapi/v2/url/sca'
 params = {'apikey': API_key, 'url':'ibm.com'}
 response = requests.post(url, data=params)
-print(response.status_code)
+print('status_code:{}'.format(response.status_code))
 print(response.text)
 #print(response.json())
